0ea15116e3d12c4f83bc9cbcddab949990e8b9ce
David Ames <readytowin2298@gmail.com>
2021-05-14 18:38:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// HINT:
+// I've attached a program called JQuery to this document. You can read it's docs at 
+// https://api.jquery.com/, though I wouldn't worry about getting too in depth with it just yet.
+
+// You can access the text inside a textBox with this pattern: $("#`idOfInput`").val()
+// You can also change the text in the box by adding a string (words between '') into .val()
+
+// Using this pattern to access the text in the subscriber option, I would say: const subscriber =  $("#subsciber").val()
+// To change subscirber i might say: $("#subscriber").val('Bill Baker')
+
+// Another helpful link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// (notice that in the lines of data, the only empty spaces are to seperate columns)
+
+
+
+// Happy coding! 
